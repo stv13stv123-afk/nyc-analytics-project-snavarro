a1e03d477c70f6cca2811f59acaf68e1e9e08a0a
Steven Navarro <steven.navarro@baruchmail.cuny.edu>
2026-02-24 00:39:12 +0000

    dim_date conformed dimension

---
+++ models/Project_work/marts/shared/dim_date.sql
@@ -0,0 +1,30 @@
+WITH all_dates AS(
+    SELECT DISTINCT CAST(created_date AS DATE) AS full_date
+    FROM {{ ref('stg_nyc_service_request_from_2020')}}
+    WHERE created_date IS NOT NULL
+    UNION DISTINCT
+
+    SELECT DISTINCT CAST(crash_date AS DATE) AS full_date
+    FROM {{ ref('stg_source_nyc_vehicle_collisions')}}
+    WHERE crash_date IS NOT NULL
+),
+date_dimension AS (
+    SELECT
+        {{dbt_utils.generate_surrogate_key(['full_date'])}} AS date_key,
+        full_date,
+        EXTRACT(YEAR FROM full_date) AS year,
+        EXTRACT(QUARTER FROM full_date) AS quarter,
+        EXTRACT(MONTH FROM full_date) AS month,
+        FORMAT_DATE('%B', full_date) AS month_name,
+        EXTRACT(DAY FROM full_date) AS day_of_month,
+        EXTRACT(DAYOFWEEK FROM full_date) AS day_of_week,
+        FORMAT_DATE('%A', full_date) AS day_name,
+        EXTRACT(DAYOFWEEK FROM full_date) IN (1,7) AS is_weekend,
+
+        CASE
+            WHEN EXTRACT(MONTH FROM full_date) >= 7 THEN EXTRACT(YEAR FROM full_date) + 1
+            ELSE EXTRACT(YEAR FROM full_date)
+        END AS fiscal_year
+    FROM all_dates
+)
+SELECT * FROM date_dimension
